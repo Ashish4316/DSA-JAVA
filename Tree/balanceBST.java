998e3349d2602bdad908a70004a84b75339fb716
Ashish <yadavashish72408@gmail.com>
2026-02-09 13:08:32 +0530

    LeetCode 1382 balancing binary Search Tree

---
+++ Tree/balanceBST.java
@@ -0,0 +1,41 @@
+package Tree;
+
+import java.util.*;
+
+public class balanceBST {
+    public class TreeNode {
+           int val;
+           TreeNode left;
+           TreeNode right;
+           TreeNode() {}
+           TreeNode(int val) { this.val = val; }
+           TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+           }
+    }
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        return build(list,0,list.size()-1);
+    }
+    public void inorder(TreeNode root,List<Integer> list){
+        if(root == null){
+            return;
+        }
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+    public TreeNode build(List<Integer> list,int l,int r){
+        if(l > r){
+            return null;
+        }
+        int mid = l + (r - l)/2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = build(list,l,mid-1);
+        root.right = build(list,mid + 1,r);
+        return root;
+    }
+}
