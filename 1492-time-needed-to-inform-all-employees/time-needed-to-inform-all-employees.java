f61f1df977f292524b23e1150779419f9bfb92d4
Ashish Kumar yadav <122154077+Ashish4316@users.noreply.github.com>
2026-03-30 23:04:18 +0530

    Time: 57 ms (61.07%) | Memory: 128.6 MB (40.85%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -14,11 +14,13 @@ class Solution {
         int ans = 0;
         while(!q.isEmpty()){
             int size = q.size();
-            int iD = q.poll();
-            for(int sub : Tree.get(iD)){
-                subSum[sub] = subSum[iD] + informTime[iD];
-                ans = Math.max(ans,subSum[sub]);
-                q.add(sub);
+            while(size-- > 0){
+                int iD = q.poll();
+                for(int sub : Tree.get(iD)){
+                    subSum[sub] = subSum[iD] + informTime[iD];
+                    ans = Math.max(ans,subSum[sub]);
+                    q.add(sub);
+                }
             }
         }
         return ans;
