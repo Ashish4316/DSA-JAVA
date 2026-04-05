08f66d76dad4f5fb81fed278bcc22b9261669509
Ashish Kumar yadav <122154077+Ashish4316@users.noreply.github.com>
2026-04-05 15:28:39 +0530

    Time: 269 ms (63.57%) | Memory: 156.5 MB (68.72%) - LeetSync

--- 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.java
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.java
@@ -2,21 +2,20 @@ class Solution{
     public List<Integer> findGoodIntegers(int n){
         int root = (int) Math.cbrt(n);
         HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        List<Integer> ans = new ArrayList<>();
+        TreeSet<Integer> set2 = new TreeSet<>();
         for(int i = 1;i <= root;i++){
             for(int j = i+1;j <= root;j++){
                 int sum = (int)Math.pow(i,3) + (int)Math.pow(j,3);
+                if(sum > n){
+                    break;
+                }
                 if(set.contains(sum) && sum <= n){
                     set2.add(sum);
                 }
                 set.add(sum);
             }
         }
-        for(int v : set2){
-            ans.add(v);
-        }
-        Collections.sort(ans);
+        List<Integer> ans = new ArrayList<>(set2);
         return ans;
     }
 }
