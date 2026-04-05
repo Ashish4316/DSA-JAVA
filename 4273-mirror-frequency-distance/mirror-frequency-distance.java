78b7476a64e4344a80554a711b9bf1ec4b74335d
Ashish Kumar yadav <122154077+Ashish4316@users.noreply.github.com>
2026-04-05 13:32:13 +0530

    Time: 195 ms (24.93%) | Memory: 63.8 MB (32.10%) - LeetSync

--- 4273-mirror-frequency-distance/mirror-frequency-distance.java
+++ 4273-mirror-frequency-distance/mirror-frequency-distance.java
@@ -1,29 +1,28 @@
 class Solution {
     public int mirrorFrequency(String s) {
         HashMap<Character, Integer> n = new HashMap<>();
-        HashSet<String> set = new HashSet<>();
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             n.put(ch, n.getOrDefault(ch, 0) + 1);
         }
         int sum = 0;
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
+        for(char ch : new HashSet<>(n.keySet())){
+            int f1 = n.getOrDefault(ch, 0);
+            if(f1 == 0){
+                continue;
+            }
             char mirror = ch;
             if(ch >= 'a' && ch <= 'z'){
                 mirror = (char) ('a' + (25 - (ch - 'a')));
             } else if(ch >= '0' && ch <= '9'){
                 mirror = (char) ('0' + (9 - (ch - '0')));
             }
-            String key1 = ch + "#" + mirror;
-            String key2 = mirror + "#" + ch;
-            if(set.contains(key1)){
-                continue;
-            }
-            sum += Math.abs(n.getOrDefault(ch, 0) - n.getOrDefault(mirror, 0));
-            set.add(key1);
-            set.add(key2);
+            int f2 = n.getOrDefault(mirror, 0);
+            sum += Math.abs(f1 - f2);
+            n.put(ch, 0);
+            n.put(mirror, 0);
         }
+
         return sum;
     }
 }
