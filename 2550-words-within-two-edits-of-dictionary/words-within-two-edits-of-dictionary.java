ad8bfa171ee272310377386458abc464035e4361
Ashish Kumar yadav <122154077+Ashish4316@users.noreply.github.com>
2026-04-22 21:04:22 +0530

    Time: 6 ms (72.79%) | Memory: 44.6 MB (32.35%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -8,6 +8,7 @@ class Solution {
                 int edits = 0;
                 for(int i = 0;i < a.length();i++){
                     if(a.charAt(i) != b.charAt(i)) edits++;
+                    if(edits > 2) break;
                 }
                 if(edits <= 2){
                    answer.add(a);
