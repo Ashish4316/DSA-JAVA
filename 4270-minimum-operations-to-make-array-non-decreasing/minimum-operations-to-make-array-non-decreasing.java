d5e144544bded22cb0424399cc2ab00dc8c1f108
Ashish Kumar yadav <122154077+Ashish4316@users.noreply.github.com>
2026-04-27 18:57:53 +0530

    Time: 4 ms (0.00%) | Memory: 87.5 MB (0.00%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.java
@@ -2,7 +2,7 @@ class Solution {
     public long minOperations(int[] nums) {
         long ans = 0;
         for(int i = 0;i < nums.length-1;i++){
-            if(nums[i] - nums[i+1] > 0) ans += (long)(nums[i] - nums[i+1]); 
+            ans += Math.max((long)(nums[i] - nums[i+1]),0); 
         }
         return ans;
     }
